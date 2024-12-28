72264c00b1dc8e58d1211831faf981a7d35d4004
Carlos Henrique <ichfs9108@gmail.com>
2024-12-27 23:45:28 -0300

    project created

---
+++ smartstock/src/main/java/tech/buildrun/smartstock/SmartstockApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.smartstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartstockApplication.class, args);
+	}
+
+}
