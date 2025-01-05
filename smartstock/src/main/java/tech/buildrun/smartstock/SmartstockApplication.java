373932ce3eef8fa8322b0a1c1836d8910c2ad424
Carlos Henrique <ichfs9108@gmail.com>
2025-01-04 23:40:47 -0300

    feat: conhecendo o openfeign

--- smartstock/src/main/java/tech/buildrun/smartstock/SmartstockApplication.java
+++ smartstock/src/main/java/tech/buildrun/smartstock/SmartstockApplication.java
@@ -1,13 +1,25 @@
 package tech.buildrun.smartstock;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import tech.buildrun.smartstock.client.GoogleClient;
 
 @SpringBootApplication
-public class SmartstockApplication {
+@EnableFeignClients
+public class SmartstockApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartstockApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(SmartstockApplication.class, args);
+    }
+
+	@Autowired
+	private GoogleClient googleClient;
+
+	@Override
+	public void run(String... args) throws Exception {
+		googleClient.helloGoogle();
 	}
-
 }
