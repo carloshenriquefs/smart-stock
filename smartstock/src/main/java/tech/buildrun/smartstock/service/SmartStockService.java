556cdcfb136dadd69b091d63f154a2fd3fda5c7d
Carlos Henrique <ichfs9108@gmail.com>
2025-01-09 02:12:49 -0300

    feat: finalizando a implementacao do fluxo do smart stock

--- smartstock/src/main/java/tech/buildrun/smartstock/service/SmartStockService.java
+++ smartstock/src/main/java/tech/buildrun/smartstock/service/SmartStockService.java
@@ -2,18 +2,23 @@ package tech.buildrun.smartstock.service;
 
 import org.springframework.stereotype.Service;
 import tech.buildrun.smartstock.domain.CsvStockItem;
+import tech.buildrun.smartstock.entity.PurchaseRequestEntity;
+import tech.buildrun.smartstock.repository.PurchaseRequestRepository;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 public class SmartStockService {
 
     private final ReportService reportService;
     private final PurchaseSectorService purchaseSectorService;
+    private final PurchaseRequestRepository purchaseRequestRepository;
 
-    public SmartStockService(ReportService reportService, PurchaseSectorService purchaseSectorService) {
+    public SmartStockService(ReportService reportService, PurchaseSectorService purchaseSectorService, PurchaseRequestRepository purchaseRequestRepository) {
         this.reportService = reportService;
         this.purchaseSectorService = purchaseSectorService;
+        this.purchaseRequestRepository = purchaseRequestRepository;
     }
 
     public void start(String reporthPath) {
@@ -27,13 +32,34 @@ public class SmartStockService {
 
                     var reorderQuantity = calculateReorderQuantity(item);
 
-                    purchaseSectorService.sendPurchaseRequest(item, reorderQuantity);
+                    var purchasedWithSuccess = purchaseSectorService.sendPurchaseRequest(item, reorderQuantity);
+
+                    persist(item, reorderQuantity, purchasedWithSuccess);
                 }
             });
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void persist(CsvStockItem item,
+                         Integer reorderQuantity,
+                         boolean purchasedWithSuccess) {
+
+        var entity = new PurchaseRequestEntity();
+        entity.setItemId(item.getItemId());
+        entity.setItemName(item.getItemName());
+        entity.setQuantityOnStock(item.getQuantity());
+        entity.setReorderThreshold(item.getReorderThreshold());
+        entity.setSupplierName(item.getSupplierName());
+        entity.setLastStockUpdateTime(LocalDateTime.parse(item.getLastStockUpdateTime()));
+
+        entity.setPurchaseQuantity(reorderQuantity);
+        entity.setPurchasedWithSuccess(purchasedWithSuccess);
+        entity.setPurchaseDateTime(LocalDateTime.now());
+
+        purchaseRequestRepository.save(entity);
     }
 
     private Integer calculateReorderQuantity(CsvStockItem item) {
